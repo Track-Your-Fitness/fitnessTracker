2b9f93020ecce9380529459952982cbcc422dcc6
Ryan Papsin <113944887+rpap75@users.noreply.github.com>
2023-03-15 12:29:27 -0700

    AWS synced to app / tables in dynamo db diplaying

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "fitnesstracker": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
