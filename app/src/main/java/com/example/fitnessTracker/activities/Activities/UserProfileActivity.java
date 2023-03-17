cdb7c3eee76622ccabcceeeabfabbfbcca941b3d
adrian.hernandez1993@yahoo.com <adrianhernandez@Adrians-MacBook-Pro-2.local>
2023-03-16 20:45:20 -0700

    Hopefully fixed errors

--- app/src/main/java/com/example/fitnessTracker/activities/Activities/UserProfileActivity.java
+++ app/src/main/java/com/example/fitnessTracker/activities/Activities/UserProfileActivity.java
@@ -154,7 +154,7 @@ public class UserProfileActivity extends AppCompatActivity {
 
                         }
                     }
-                }
+                } 
         );
         return imagePickingActivityResultLauncher;
     }
